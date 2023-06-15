5890de0072ae4a177afa84ec3175daee76405857
Danilo Verone <dan.verone@hotmail.com>
2023-06-15 16:39:15 -0300

    Alterado para ser php

---
+++ index.php
@@ -0,0 +1,153 @@
+<?php require_once "../sessao.php"; ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Validação de Campos</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f1f1f1;
+        }
+        
+        .container {
+            max-width: 500px;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: #fff;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            border-radius: 5px;
+        }
+        
+        h1 {
+            text-align: center;
+        }
+        
+        textarea {
+            width: 100%;
+            height: 150px;
+            padding: 10px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            resize: vertical;
+        }
+        
+        button {
+            display: block;
+            margin: 10px auto;
+            padding: 10px 20px;
+            font-size: 16px;
+            color: #fff;
+            background-color: #007bff;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+        
+        button:hover {
+            background-color: #0056b3;
+        }
+        
+        .error-message {
+            color: red;
+            font-weight: bold;
+            margin-top: 10px;
+            padding: 10px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            resize: vertical;
+            width: 100%;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Validação de Campos</h1>
+
+        <textarea id="requestDataInput" rows="10" placeholder="Cole a solicitação GET aqui..."></textarea>
+        <br>
+        <label for="selection">Escolha o Endpoint:</label>
+        <select id="selection">
+            <option value="brands">Brands</option>
+            <option value="categories">Categories</option>
+            <option value="images">Images</option>
+            <option value="products">Products</option>
+            <option value="sku">Sku</option>
+            <option value="stocks">Stocks</option>
+            <option value="variations">Variations</option>
+            </select>
+
+        <button onclick="validateRequest()">Validar</button>
+
+        <div id="errorMessageContainer" class="error-message" style="display: none;"></div>
+
+        <script>
+            function validateRequest() {
+                var requestData = document.getElementById("requestDataInput").value;
+
+                try {
+                    var data = JSON.parse(requestData);
+
+                    if (typeof data === 'object' && data !== null) {
+                        var expectedData;
+                        var missingFields = [];
+
+                        // Verifica qual parte do payload foi selecionada
+                        var selection = document.getElementById("selection");
+                        var selectedValue = selection.options[selection.selectedIndex].value;
+
+                        if (selectedValue === "brands") {
+                            expectedData = {
+                                'name': '',
+                                'reducedName': '',
+                                'partnerId': ''
+                            };
+                        } else if (selectedValue === "products") {
+                            expectedData = {
+                                'id': '',
+                                'title': '',
+                                'description': '',
+                                'externalIdProduct': ''
+                            };
+                        } else if (selectedValue === "stocks") {
+                            expectedData = {
+                                'id': '',
+                                'partnerId': '',
+                                'quantity': '',
+                                'cost': '',
+                                'additionalTime': '',
+                                'stockLocalId': ''
+                            };
+                        } else {
+                            console.log("Escolha inválida");
+                            return;
+                        }
+
+                        // Verifica se cada campo esperado está presente no objeto recebido
+                        for (var field in expectedData) {
+                            if (!data.hasOwnProperty(field)) {
+                                missingFields.push(field);
+                            }
+                        }
+
+                        // Verifica se há campos ausentes e exibe a mensagem adequada
+                        if (missingFields.length === 0) {
+                            alert('Todos os campos estão presentes conforme o esperado.');
+                        } else {
+                            var errorMessage = 'Os seguintes campos estão ausentes: ' + missingFields.join(', ') + '.';
+                            document.getElementById("errorMessageContainer").innerHTML = errorMessage;
+                            document.getElementById("errorMessageContainer").style.display = "block";
+                        }
+                    } else {
+                        alert('A solicitação precisa ser um objeto JSON válido.');
+                    }
+                } catch (error) {
+                    alert('Erro ao analisar a solicitação. Verifique se é uma solicitação GET válida em formato JSON.');
+                }
+            }
+        </script>
+    </div>
+</body>
+
+</html>
